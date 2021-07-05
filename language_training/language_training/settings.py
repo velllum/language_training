2b28280d0cbe999f09bc03eb18876517a0c722cc
velllum <velllum@yandex.ru>
2021-07-06 00:57:19 +0300

    Добавить миксины

--- language_training/language_training/settings.py
+++ language_training/language_training/settings.py
@@ -15,11 +15,10 @@ from pathlib import Path
 
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
