d812f67ccae827109d2be7c994b10f9e4da27081
velllum <velllum@yandex.ru>
2021-08-05 01:14:14 +0300

    Воспроизвести список слов

--- language_training/translator/static/js/scripts.js
+++ language_training/translator/static/js/scripts.js
@@ -1,15 +1,29 @@
+//function playAllWords(){
+//    var audio = document.getElementsByTagName("audio");
+//    audio.play()
+//    for (let i = 0; i < audio.length; i++) {
+//      audio[i].addEventListener('ended', function() {
+//        if (audio[i].duration === audio[i].currentTime) {
+//          audio[i + 1].play();
+//        }
+//      });
+//    }
+//}
+
+
+
 
 /* Подстановка перевода, при клике по кнопке */
-$(function() {
-    $('.hidden').toggle();
-    $("#desktopSwitch, #mobileSwitch").click(function () {
-        if ($('.show').is(":visible")) {
-            $('.show').toggle();
-            $('.hidden').toggle();
-        } else {
-            $('.hidden').toggle();
-            $('.show').toggle();
-        }
-    });
-});
+//$(function() {
+//    $('.hidden').toggle();
+//    $("#desktopSwitch, #mobileSwitch").click(function () {
+//        if ($('.show').is(":visible")) {
+//            $('.show').toggle();
+//            $('.hidden').toggle();
+//        } else {
+//            $('.hidden').toggle();
+//            $('.show').toggle();
+//        }
+//    });
+//});
 
