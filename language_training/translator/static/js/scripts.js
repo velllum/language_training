017f2f6475eede962c0616a5a12e5380fd9b21f1
velllum <velllum@yandex.ru>
2021-08-09 01:12:50 +0300

    Воспроизвести список слов

--- language_training/translator/static/js/scripts.js
+++ language_training/translator/static/js/scripts.js
@@ -1,94 +1,214 @@
-var tagAudio = document.getElementsByTagName("audio");
-var arrayAudio = []
-var host = window.location.origin;
 
 
-for (let a of tagAudio) {
-    arrayAudio.push(a.getAttribute('src'))
-}
-
-console.log(arrayAudio, host, host.concat(arrayAudio[0]));
-
-//$('input').click(function() {
-//  audio.src = this.name
-//  audio.play();
-//})
-function playAllWords(task){
-    var audio = new Audio();
-    var current = 0;
-    audio.src = host.concat(arrayAudio[0]);
-    //player.autoplay = true;
-    console.log(audio.src)
-    audio.play();
-}
-//player.onended = function() {
-//  current++;
-//  if (current >= arrayAudio.length) current = 0;
-//  player.src = arrayAudio[current];
-//  player.play();
+//var tagAudio = document.getElementsByTagName("audio");
+//var arrayAudio = []
+//
+//for (let a of tagAudio) {
+//    arrayAudio.push(a.getAttribute('src'))
+//}
+//
+//console.log(arrayAudio);
+//
+//function playAllWords(task) {
+//
+//    var audio = new Audio();
+//
+//    if (task === "play") {
+//
+//        var current = 0;
+//        audio.src = arrayAudio[0];
+//        audio.autoplay = true;
+//
+//        audio.onended = function() {
+//            if (current >= arrayAudio.length) {
+//                console.log("stop")
+//                this.stop();
+//            } else if (task === "play") {
+//                console.log("play")
+//                current++;
+//                this.src = arrayAudio[current];
+//                this.play();
+//            }
+//        }
+//    }
+//
+//    Audio.prototype.stop = function() {
+//        console.log("Audio.prototype.stop")
+//        this.pause();
+//        this.currentTime = 0;
+//    }
+//
+//    if (task === "pause") {
+//        console.log("stop")
+//        this.stop();
+//    }
 //}
 
 
+
+//var tagAudio = document.getElementsByTagName("audio");
+//var arrayAudio = []
+//
+//for (let a of tagAudio) {
+//    arrayAudio.push(a.getAttribute('src'))
+//}
+//
+//console.log(arrayAudio);
+//
 //function playAllWords(task){
-//    var audio = document.getElementsByTagName("audio").textContent;
-//    console.log(audio)
-//    var el = document.getElementById('desktopSwitch');
-//    var length = audio.length - 1;
-//    var playing = false; // текущее состояние плеера
-////    var count = 0;
+////    var audio = new Audio();
+//    var length = arrayAudio.length;
 //
-//    console.log(audio, task)
+////    console.log(audio, task)
+//    var audio = new Audio();
 //
-//    var array = []
+//    audio.src = arrayAudio[0];
+//    audio.autoplay = true;
+//    audio.preload = "auto";
+//
 //    for (let i = 0; i < length; i++) {
+//        audio.src = arrayAudio[i];
+//        audio.play();
+//        audio.addEventListener('ended', function() {
 //
+//            audio.play();
 //
-//
+//        }, false);
 //    }
 //
-//    for (let i = 0; i < length; i++) {
-//        audio[i].addEventListener('ended', function() {
 //
-//
-//            el.addEventListener('click', function(){
-//
-//                if (task == "play"){
-//                    if (i == undefined){
-//                        audio[0].play();
-//                    } else {
-//                        audio[i].play();
-//                    }
-//
-//                }
-//
-//                if (task == "pause"){
-//                    audio[i].pause();
-//                }
-//
-//            });
-//
-//
-//            console.log(i, audio.length)
-//            audio[i + 1].play();
-//
-//        });
-//
+//    Audio.prototype.stop = function() {
+//        console.log("Audio.prototype.stop")
+//        this.pause();
+//        this.currentTime = 0;
 //    }
 //}
 
 
-/* Подстановка перевода, при клике по кнопке */
-$(function() {
-    $('.btn-hidden').toggle();
-    $("#desktopSwitch, #mobileSwitch").click(function () {
-        if ($('.btn-show').is(":visible")) {
-            $('.btn-show').toggle();
-            $('.btn-hidden').toggle();
-        } else {
-            $('.btn-hidden').toggle();
-            $('.btn-show').toggle();
-        }
-    });
+
+
+//var tagAudio = document.getElementsByTagName("audio");
+//var array = []
+//
+//for (let a of tagAudio) {
+//    array.push(a.getAttribute('src'))
+//}
+//
+//var el = document.getElementById('player');
+//var playing = false; // текущее состояние плеера
+//
+//var player = new Audio();
+//
+////function playAllWords(task){
+//for (let i = 0; i < array.length; i++) {
+//    player.src = array[i]
+//    player.preload = "auto";
+//
+//
+//player.addEventListener('ended', function(){ // слушаем окончание трека
+////    el.innerText = "Done";
+//    console.log("до", playing)
+//    playing = false;
+//    console.log("после", playing)
+//});
+//}
+//el.addEventListener('click', function(){
+//
+////    for (let i = 0; i < arrayAudio.length; i++) {
+//
+//            player.preload = "auto";
+//            console.log("player.ended", player.ended)
+//    //        player.load();
+//            if(playing){
+//                player.pause();
+//            //        el.innerText = "Paused";
+//            } else {
+//
+//    //            player.cloneNode(true).play();
+//            player.play();
+//            //        el.innerText = "Playing..";
+//            }
+//
+//    //    }
+//        playing = !playing;
+//        console.log("playing = !playing;", playing)
+//
+//
+////    }
+//}); // слушаем нажатие на кнопку
+//
+//
+///* Подстановка перевода, при клике по кнопке */
+//$(function() {
+//    $('.btn-hidden').toggle();
+//    $("#desktopSwitch, #mobileSwitch").click(function () {
+//        if ($('.btn-show').is(":visible")) {
+//            $('.btn-show').toggle();
+//            $('.btn-hidden').toggle();
+//        } else {
+//            $('.btn-hidden').toggle();
+//            $('.btn-show').toggle();
+//        }
+//    });
+//});
+
+
+
+
+
+//var audio = document.getElementsByTagName("audio");
+//var array = []
+//
+//for (let a of audio) {
+//    array.push(a.getAttribute('src'))
+//}
+//
+//iBtn=document.getElementsByTagName('button');
+//
+//
+//for(j = 0; j < audio.length; j++)
+//{
+//    audio[j].addEventListener
+//    (
+//        'click',
+//        function()
+//        {
+////            var audio=new Audio(this.id+'.mp3');
+//
+//            if(audio[j].paused)
+//            {
+//                audio[j].play();
+////                iBtn[j].classList.remove("play");
+////                iBtn[j].classList.add("pause");
+//            }
+//            else
+//            {
+//                audio[j].pause();
+////                iBtn[j].classList.remove("pause");
+////                iBtn[j].classList.add("play");
+//            }
+//
+//        },false
+//    );
+//}
+
+
+
+
+function onlyPlayOneIn(container) {
+  container.addEventListener("play", function(event) {
+  audio_elements = container.getElementsByTagName("audio")
+    for(i=0; i < audio_elements.length; i++) {
+      audio_element = audio_elements[i];
+      if (audio_element !== event.target) {
+        audio_element.pause();
+      }
+    }
+  }, true);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  onlyPlayOneIn(document.body);
 });
 
 
